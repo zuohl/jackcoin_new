47a5469cf2494655e7cb6608d9283a2cca9daaa5
zuohl <wozixunwodao@qq.com>
2016-06-19 23:04:33 +0800

    提交

---
+++ src/main/java/com/jackcoin/bean/Manager.java
@@ -0,0 +1,26 @@
+package com.jackcoin.bean;
+
+/**
+ * Created by zuohl on 2016/6/19.
+ */
+public class Manager {
+    private String userName;
+
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -4,3 +4,26 @@
 var RESULT_SUCCESS = 0;
 var RESULT_BUSINESS_ERROR = 10;
 var RESULT_EXCEPTION = 100;
+
+function isNotEmpty(_str) {
+    var tmp_str = jQuery.trim(_str);
+    if (tmp_str && typeof(tmp_str)!="undefined" && tmp_str!="") {
+        return true;
+    }
+    return false;
+}
+
+var setValue = function (key,value) {
+    localStorage.setItem(key,value);
+}
+
+/**
+ * 获取分页key值
+ * @param key
+ * @returns {number}
+ */
+var getValue = function (key) {
+    var value = localStorage.getItem(key)
+    return isNotEmpty(value)?value:"";
+}
+
